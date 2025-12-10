100875b774cd8baa73530ddd3e5383d93a02087d
emyreharris3 <emyre.harris@inspire35.org>
2025-12-10 19:09:09 +0000

    hgfjdk

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -50,7 +50,7 @@ var init = function (window) {
 
 
         // TODO 7 : Use a loop to create multiple circles
-        for (var i = 0; i < 100; i++){
+        for (var i = 0; i < 150; i++){
             drawCircle();
         }
 
@@ -82,6 +82,7 @@ var init = function (window) {
             // TODO 8 / TODO 9 : Iterate over the array
            for (var i = 0; i < circles.length; i++) {
            physikz.updatePosition(circles[i]);
+           game.checkCirclePosition(circles[i]);
 }
             
         }
@@ -99,7 +100,23 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
+           if (circle.x > canvas.width) {
+            circle.x = 0;
+           }
+
+           if (circle.x < 0) {
+            circle.x = canvas.width;
+           }
+           if(circle.y > canvas.height) {
+            circle.y = 0;
+           }
+           if (circle.y < 0) {
+            circle.y = canvas.height;
+           }
+
+
+
+
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
         
